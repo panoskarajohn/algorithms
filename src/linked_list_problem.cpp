b1c2cefddfbd408f4adedd70329c8a3f109481aa
panagiotiskaragiannis <panoskarajohb@hotmail.com>
2026-02-08 15:39:05 +0000

    feat: milddle of linkedlist

--- src/linked_list_problem.cpp
+++ src/linked_list_problem.cpp
@@ -1,3 +1,16 @@
 #include "linked_list_problem.h"
 
-std::shared_ptr<Node<int>> middleNode(const std::shared_ptr<Node<int>> &head) { return nullptr; }
+std::shared_ptr<Node<int>> middleNode(const std::shared_ptr<Node<int>> &head) {
+    if (head == nullptr) {
+        return head;
+    }
+
+    auto slow = head;
+    auto fast = head;
+    while (fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
