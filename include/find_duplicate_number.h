974de8175d58fffb86717eb754bcffb4128bbd40
panagiotiskaragiannis <panoskarajohb@hotmail.com>
2026-02-10 21:23:55 +0000

    unorthodox way.... but it was in the spirit of linked list

--- include/find_duplicate_number.h
+++ include/find_duplicate_number.h
@@ -4,4 +4,4 @@
 
 // Returns the duplicated number in an array containing n + 1 integers
 // where each integer is in the range [1, n], inclusive.
-int findDuplicate(const std::vector<int> &nums);
+int findDuplicate(std::vector<int> &nums);
--- src/find_duplicate_number.cpp
+++ src/find_duplicate_number.cpp
@@ -1,7 +1,14 @@
 #include "find_duplicate_number.h"
 #include <stdexcept>
 
-int findDuplicate(const std::vector<int> &nums) {
-    (void)nums;
-    throw std::logic_error("TODO: implement findDuplicate");
+int findDuplicate(std::vector<int> &nums) {
+    for (int i = 0; i < nums.size(); i++) {
+        int index = abs(nums[i]);
+        if (nums[index] < 0) {
+            return index;
+        }
+        nums[index] = nums[index] * -1;
+    }
+
+    return -1; // we should never reach here based on the problem description
 }
